03077749a18ad79084757af549ddd8f0ad675524
Nauman Javed Qureshi <106303413+Nauman-Javed@users.noreply.github.com>
2024-04-14 18:55:10 +0500

    added inital pages

---
+++ FrontEnd/src/pages/signup.tsx
@@ -0,0 +1,12 @@
+import { SignupPage } from "@/collection";
+import React from "react";
+
+const signup = () => {
+  return (
+    <div>
+      <SignupPage />
+    </div>
+  );
+};
+
+export default signup;
